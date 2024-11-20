4a8e02fa1afe8dcd45697288ec59823d5db17b50
Deeppanchal2108 <deepanchal2290@gmail.com>
2024-11-20 11:56:37 +0530

    Added the logout functionality

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React from 'react'
-import { useSession } from 'next-auth/react'
+import { useSession ,signOut} from 'next-auth/react'
 function page() {
   const { data: session } = useSession();
  
@@ -15,9 +15,14 @@ function page() {
       <h1>{session.user.role == "Admin" ? "You are on the admin route" : session.user.role == "Doctor"?"You are on doctor route":"You are on Patient route "}</h1>
       <h3>Session Id  : {session.user.id||"Not getting id don`t know why"}</h3>
       <h3>Session Email : {session.user.email}</h3>
-      {/* <h3>Session Password : {session.user.password|| "Can`t show the password"} </h3> */}
-      <h3>Session Role  : {session.user.role||"Unable to load the role"}</h3>
+      <h3>Session Password : {session.user.password|| "Can`t show the password"} </h3>
+      <h3>Session Role  : {session.user.role || "Unable to load the role"}</h3>
+      <button className='bg-red-500 text-white rounded-lg p-2 m-4' onClick={()=>signOut({callbackUrl:"/signin"})}>
+        LogOut 
+      </button>
     </div>
+
+
   )
 }
 
--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
-import Image from "next/image";
-
+import Link from "next/link";
 export default function Home() {
   return (
     <div className="p-10">
-      Deep panchal
+      Home Page 
+      
+      <Link href={"/signup"}>
+      <button className=" bg-white text-black p-2 rounded-md m-2 ">
+        SignUp 
+        </button>
+      </Link>
+      <Link href={"/signin"}>
+      <button className=" bg-white text-black p-2 rounded-md m-2">
+        SignIn
+        </button>
+      </Link>
     </div>
   );
 }
